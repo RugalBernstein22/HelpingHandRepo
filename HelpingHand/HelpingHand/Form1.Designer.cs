67985d12e0f8e98502f4c70ef0955af2033e4fec
RickWaaijer <koelkast2460@gmail.com>
2020-12-08 13:05:02 +0100

    Edit design 3

--- HelpingHand/HelpingHand/Form1.Designer.cs
+++ HelpingHand/HelpingHand/Form1.Designer.cs
@@ -40,8 +40,10 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button13 = new System.Windows.Forms.Button();
+            this.companies1 = new HelpingHand.Companies();
             this.mySecondCustmControl1 = new HelpingHand.MySecondCustmControl();
             this.firstCustomControl1 = new HelpingHand.FirstCustomControl();
+            this.settings2 = new HelpingHand.Settings();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -57,18 +59,18 @@
             this.panel1.Controls.Add(this.button1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(314, 897);
+            this.panel1.Size = new System.Drawing.Size(279, 718);
             this.panel1.TabIndex = 0;
             // 
             // SidePanel
             // 
             this.SidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.SidePanel.Location = new System.Drawing.Point(2, 94);
-            this.SidePanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SidePanel.Location = new System.Drawing.Point(2, 75);
+            this.SidePanel.Margin = new System.Windows.Forms.Padding(4);
             this.SidePanel.Name = "SidePanel";
-            this.SidePanel.Size = new System.Drawing.Size(15, 83);
+            this.SidePanel.Size = new System.Drawing.Size(13, 66);
             this.SidePanel.TabIndex = 4;
             // 
             // button7
@@ -79,14 +81,15 @@
             this.button7.ForeColor = System.Drawing.Color.White;
             this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
             this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button7.Location = new System.Drawing.Point(18, 804);
-            this.button7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button7.Location = new System.Drawing.Point(16, 643);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(296, 83);
+            this.button7.Size = new System.Drawing.Size(263, 66);
             this.button7.TabIndex = 4;
             this.button7.Text = "       settings";
             this.button7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button3
             // 
@@ -96,14 +99,15 @@
             this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(18, 257);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Location = new System.Drawing.Point(16, 206);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(296, 83);
+            this.button3.Size = new System.Drawing.Size(263, 66);
             this.button3.TabIndex = 4;
             this.button3.Text = "       Companies";
             this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -113,10 +117,10 @@
             this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(18, 174);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Location = new System.Drawing.Point(16, 139);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(296, 83);
+            this.button2.Size = new System.Drawing.Size(263, 66);
             this.button2.TabIndex = 4;
             this.button2.Text = "       Jobs";
             this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -131,10 +135,10 @@
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(18, 90);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(16, 72);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(296, 83);
+            this.button1.Size = new System.Drawing.Size(263, 66);
             this.button1.TabIndex = 4;
             this.button1.Text = "       Home";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -145,30 +149,30 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(314, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Location = new System.Drawing.Point(279, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1225, 15);
+            this.panel2.Size = new System.Drawing.Size(1089, 12);
             this.panel2.TabIndex = 1;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.panel3.Controls.Add(this.pictureBox1);
-            this.panel3.Location = new System.Drawing.Point(364, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel3.Location = new System.Drawing.Point(324, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(166, 220);
+            this.panel3.Size = new System.Drawing.Size(148, 176);
             this.panel3.TabIndex = 2;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.Image = global::HelpingHand.Properties.Resources.HelpingHand_Logo;
-            this.pictureBox1.Location = new System.Drawing.Point(-1, 23);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 18);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(167, 188);
+            this.pictureBox1.Size = new System.Drawing.Size(148, 150);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -177,10 +181,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(540, 30);
+            this.label4.Location = new System.Drawing.Point(480, 24);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 30);
+            this.label4.Size = new System.Drawing.Size(0, 23);
             this.label4.TabIndex = 3;
             // 
             // button13
@@ -191,35 +195,51 @@
             this.button13.ForeColor = System.Drawing.Color.White;
             this.button13.Image = ((System.Drawing.Image)(resources.GetObject("button13.Image")));
             this.button13.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button13.Location = new System.Drawing.Point(1466, 30);
-            this.button13.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button13.Location = new System.Drawing.Point(1303, 24);
+            this.button13.Margin = new System.Windows.Forms.Padding(4);
             this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(48, 54);
+            this.button13.Size = new System.Drawing.Size(43, 43);
             this.button13.TabIndex = 4;
             this.button13.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button13.UseVisualStyleBackColor = true;
             // 
+            // companies1
+            // 
+            this.companies1.Location = new System.Drawing.Point(278, 218);
+            this.companies1.Name = "companies1";
+            this.companies1.Size = new System.Drawing.Size(1090, 500);
+            this.companies1.TabIndex = 7;
+            // 
             // mySecondCustmControl1
             // 
-            this.mySecondCustmControl1.Location = new System.Drawing.Point(316, 240);
-            this.mySecondCustmControl1.Margin = new System.Windows.Forms.Padding(9, 10, 9, 10);
+            this.mySecondCustmControl1.Location = new System.Drawing.Point(281, 192);
+            this.mySecondCustmControl1.Margin = new System.Windows.Forms.Padding(8);
             this.mySecondCustmControl1.Name = "mySecondCustmControl1";
-            this.mySecondCustmControl1.Size = new System.Drawing.Size(1227, 625);
+            this.mySecondCustmControl1.Size = new System.Drawing.Size(1091, 500);
             this.mySecondCustmControl1.TabIndex = 6;
             // 
             // firstCustomControl1
             // 
-            this.firstCustomControl1.Location = new System.Drawing.Point(318, 240);
-            this.firstCustomControl1.Margin = new System.Windows.Forms.Padding(9, 10, 9, 10);
+            this.firstCustomControl1.Location = new System.Drawing.Point(283, 192);
+            this.firstCustomControl1.Margin = new System.Windows.Forms.Padding(8);
             this.firstCustomControl1.Name = "firstCustomControl1";
-            this.firstCustomControl1.Size = new System.Drawing.Size(1208, 650);
+            this.firstCustomControl1.Size = new System.Drawing.Size(1074, 520);
             this.firstCustomControl1.TabIndex = 5;
+            // 
+            // settings2
+            // 
+            this.settings2.Location = new System.Drawing.Point(279, 218);
+            this.settings2.Name = "settings2";
+            this.settings2.Size = new System.Drawing.Size(1090, 500);
+            this.settings2.TabIndex = 8;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1539, 897);
+            this.ClientSize = new System.Drawing.Size(1368, 718);
+            this.Controls.Add(this.settings2);
+            this.Controls.Add(this.companies1);
             this.Controls.Add(this.mySecondCustmControl1);
             this.Controls.Add(this.firstCustomControl1);
             this.Controls.Add(this.label4);
@@ -228,7 +248,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -255,6 +275,8 @@
         private FirstCustomControl firstCustomControl1;
         private MySecondCustmControl mySecondCustmControl1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private Companies companies1;
+        private Settings settings2;
     }
 }
 
--- HelpingHand/HelpingHand/Form1.cs
+++ HelpingHand/HelpingHand/Form1.cs
@@ -36,5 +36,19 @@ namespace HelpingHand
             //Thanks for watching Friends...
             //Please dont forget to Subscribe... :) :) :) 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = button3.Height;
+            SidePanel.Top = button3.Top;
+            companies1.BringToFront();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = button7.Height;
+            SidePanel.Top = button7.Top;
+            settings2.BringToFront();
+        }
     }
 }
